639a1d040608a05482facd87c3b654efc8923d39
sethcg <sethcg@hotmail.com>
2025-11-28 09:24:39 -0500

    added core library

---
+++ core/include/CustomReader.hpp
@@ -0,0 +1,13 @@
+#ifndef CUSTOM_READER_H
+#define CUSTOM_READER_H
+
+#include <iostream>
+
+namespace CustomReader {
+
+    int ReadWritePointData(std::string intput, std::string output);
+
+}
+
+
+#endif
